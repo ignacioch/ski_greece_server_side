0957520c23c180194bbaaa4fc242564d14979c26
IgnacioCh <ign_ch@hotmail.com>
2014-10-31 16:48:33 +0000

    Adding about page with links to main - part 1 completed

--- tools/skigreece_dashboard/dashboard/urls.py
+++ tools/skigreece_dashboard/dashboard/urls.py
@@ -4,6 +4,7 @@ from dashboard import views
 
 urlpatterns =  patterns (' ', 
 
-    url (r'^$',views.index, name='index')
+    url (r'^$',views.index, name='index'),
+    url (r'^about/',views.about, name='about')
 
 )   
--- tools/skigreece_dashboard/dashboard/views.py
+++ tools/skigreece_dashboard/dashboard/views.py
@@ -5,4 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request) :
-   return HttpResponse("Welcome to dashboard main page")  
+   return HttpResponse("Welcome to dashboard main page. Go to <a href = '/dashboard/about'> About page </a>") 
+
+def about(request) :
+   return HttpResponse("This is the about page. Return to <a href='/dashboard'> Main </a>") 
