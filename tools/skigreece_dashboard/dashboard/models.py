e4e7a48067df3b105a30bb02da62446b53ff25cd
IgnacioCh <ign_ch@hotmail.com>
2014-11-10 16:22:25 +0000

    removing thrash files

--- tools/skigreece_dashboard/dashboard/models.py
+++ tools/skigreece_dashboard/dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Skicenter(models.Model):
+    name        = models.CharField(max_length=255, unique=True)
+    englishName = models.CharField(max_length=255, unique=True)
+    snow_down   = models.IntegerField(default=0)
+    snow_up     = models.IntegerField(default=0)
+    openCond    = models.IntegerField(default=0)
+    weatherUrl  = models.CharField(max_length=255, unique=True)
+    website     = models.CharField(max_length=255, unique=True)
+
+    def __unicode__(self):
+        return self.name    
