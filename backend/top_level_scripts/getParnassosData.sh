06465539a8daa69e5d7c665fdebec98f3b1f1199
IgnacioCh <ign_ch@hotmail.com>
2015-01-08 19:57:33 +0000

    Updating crontab script

--- backend/top_level_scripts/getParnassosData.sh
+++ backend/top_level_scripts/getParnassosData.sh
@@ -6,3 +6,4 @@ cd /home/bitnami/htdocs/SkiGreece_201415/backend/livenews/data_parser/
 cp parnassos.log /home/bitnami/htdocs/SkiGreece_201415/backend/livenews/data_log/parnassos/parnassos_backup_$(date +%Y%m%d)_$(date +%H:%M:%S).log
 rm -rf parnassos.log
 php parnassos_data.php
+cd ~
